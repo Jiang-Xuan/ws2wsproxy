0b75df94d6297e0a7e22dcd7f093e25b9570c62c
jiangxuan <jiangxuan@dxy.cn>
2018-02-03 14:38:35 +0800

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'ws2wsproxy',
+      script    : './lib/ws2wsproxy.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {}
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+const ws2wsproxy = require('./lib/ws2wsproxy')
+
+exports.ws2wsproxy = ws2wsproxy
