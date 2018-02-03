4b19d05600bb345c3caf8dd7dcb9f3d424fb2dcd
jiangxuan <jiangxuan@dxy.cn>
2018-02-03 19:22:51 +0800

    cross env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,8 @@ module.exports = {
       name      : 'ws2wsproxy',
       script    : './lib/ws2wsproxy.js',
       env: {
-        COMMON_VARIABLE: 'true'
+        COMMON_VARIABLE: 'true',
+        NODE_ENV: 'development'
       },
       env_production : {
         NODE_ENV: 'production'
